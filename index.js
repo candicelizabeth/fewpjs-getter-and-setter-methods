8f4e67b50b71db2f3f34bead93b7b437a6c3df37
Candice Peters <candice.peters@flatironschool.com>
2021-09-19 20:23:26 -0500

    commit tree c958cf89998c151720932e9789488409431d3577

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Add your Circle class here
+class Circle {
+    constructor(radius){
+        this.radius = radius
+    }
+
+    get diameter(){
+        return this.radius * 2
+    }
+
+    get circumference(){
+        // debugger
+        
+        return this.diameter * Math.PI 
+    }
+
+    get area(){
+        return Math.PI * (this.radius)** 2
+    }
+
+    set diameter(diameter){
+        this.radius = diameter / 2 
+    }
+
+    set circumference(circumference){
+        this.radius = circumference / (2 * Math.PI)
+    }
+
+    set area(area){
+        this.radius = Math.sqrt(area / Math.PI)
+    }
+
+    // this.radius = circumference / ( pi * 2 )
+}
